db2250e7b1e8b6438e6c89c7e330c493d37b480d
YX <pig60214@gmail.com>
2024-07-11 02:56:12 +0800

    Add Swagger

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateAccountDto } from './dto/create-account.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Account')
 @Controller('account')
 export class AccountController {
   @Post()
--- src/account/dto/create-account.dto.ts
+++ src/account/dto/create-account.dto.ts
@@ -1,4 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateAccountDto {
+  @ApiProperty({ example: 'Michelle', description: 'The name of the account' })
   name: string;
+
+  @ApiProperty({ example: 10000000 })
   balance: number;
 }
